8fd3b7a5a9fc76e1ade379db4ddf68a0c313ed38
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-07-25 17:38:42 -0500

    everyting on page, needs styling

--- apps.js
+++ apps.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const app = express();
+const data = require('./data');
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
 
-//render inxed page in views
-app.get('/', function(req, rec){
-  res.render('index');
+//render index page in views
+//will render the index page and pull in the variable data.
+app.get('/', function(req, res){
+  res.render('index', data);
+})
+
+app.listen(3500, function(){
+  console.log("App is listening for you on port 3500.")
 })
