25ca2b2153303a492c619f3ff3594f34095cb985
Stefanie Boscarino <stefanie.boscarino@gmail.com>
2017-07-26 14:25:55 -0500

    everything but the css is working.

--- apps.js
+++ apps.js
@@ -3,6 +3,8 @@ const mustacheExpress = require('mustache-express');
 const app = express();
 const data = require('./data');
 
+app.use(express.static('public'));
+
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
 app.set('views', __dirname + '/views');
@@ -11,7 +13,18 @@ app.set('views', __dirname + '/views');
 //will render the index page and pull in the variable data.
 app.get('/', function(req, res){
   res.render('index', data);
+  console.log(data);
 })
+
+//gets then renders a robot to the detail.mustache page.
+app.get('/detail/:id', function(req, res){
+  let robot = data.users.find(function(item){
+    return item.id == req.params.id;
+  });
+  console.log(robot);
+  res.render('detail', robot);
+});
+
 
 app.listen(3500, function(){
   console.log("App is listening for you on port 3500.")
